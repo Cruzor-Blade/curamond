8a47037f3a9e2cc40263fa056d1fa49fd11b56b9
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2021-12-15 11:37:50 +0100

    Initialized the app and with a basic Welcome message at the AppScreen

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{alignItems:'center', justifyContent:'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App;

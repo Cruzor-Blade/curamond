6152386de4e7755350618e2b16ad796ede12216e
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2021-12-21 04:58:50 +0100

    Now the bidireactional scroll navigation is working properly

--- components/Card.js
+++ components/Card.js
@@ -1,48 +1,41 @@
 import React from 'react';
-import { Image, View, StyleSheet, Text, Dimensions } from 'react-native';
+import { Image, View, StyleSheet, Text, Dimensions, FlatList } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
 
 const windowWidth = Dimensions.get("window").width;
 
-const Card = ({number}) => {
-  const cards = [
-    require("../assets/cards/card1.png"),
-    require("../assets/cards/card2.png"),
-    require("../assets/cards/card3.png"),
-    require("../assets/cards/card4.png"),
-    require("../assets/cards/card5.png"),
-    require("../assets/cards/card6.png"),
-  ];
+const Card = ({item}) => {
+  const imgs = {
+    1:[require("../assets/images/1.png")],
+    2:[require("../assets/images/2.jpg")],
+    3:[require("../assets/images/3.jpg")],
+    4:[require("../assets/images/4.png")],
+    5:[require("../assets/images/5.png")],
+    6:[require("../assets/images/6-1.jpg"), require("../assets/images/6-2.jpg")],
+    7:[require("../assets/images/7.jpeg")],
+    };
 
-  const curiosities = [
-    {body:`Tom et Jerry s'appelaient à l'origine Jasper et Jinx dans leur premier dessin animé "Puss Gets the Boot"`},
-    {body:`80% des gens se sont retrouvés à un moment donné à chanter inconsciemment une chanson qu'ils détestent.`},
-    {body:`De nouvelles recherches ont montré que les personnes seules ont des compétences sociales supérieures par rapport aux personnes qui ne sont pas seules.`},
-    {body:`Un Rubik's Cube a 43 252 003 274 489 856 000 configurations possibles.`},
-    {body:`Votre cerveau continue de se développer jusqu'à la quarantaine`},
-    {body:`Le roi Abdallah d'Arabie Saoudite a été choqué lorsque la reine Elizabeth l'a conduit dans son domaine, car les femmes ne peuvent pas conduire dans son pays.`},
-    {body:`Soyez une version de premier ordre de vous même, pas une version de second ordre de quelqu'un d'autre.`}
-  ]
+    const images = imgs[item.images]
 
   return (
-    <View style={styles.card}>
-      {/* <Image style={styles.cardImgStyles} source={cards[number]} /> */}
-      <Text style={styles.body}>{curiosities[number].body}</Text>
-    </View>
+   <View style={styles.card}>
+      <ScrollView pagingEnabled={true}>
+        <View style={styles.bodyCard} ><Text style={styles.body}>{item.body}</Text></View>
+        {images.map (image => <Image source={image} style={{width:windowWidth*11/13, maxHeight:410, resizeMode:'contain'}} />)}
+      </ScrollView>
+   </View>
   )
 }
 
 export default Card;
 
-const styles= StyleSheet.create({
-  cardImgStyles: {
-    height:400,
-    width:600,
-    resizeMode:'contain',
-  },
+const styles = StyleSheet.create({
   card:{
     width:windowWidth*(12/13),
-    height:430,
-    borderWidth:0.5,
+    maxHeight:430,
+    borderWidth:1,
     borderColor:"#333333",
     elevation:8,
     shadowOpacity:0.8,
@@ -54,11 +47,20 @@ const styles= StyleSheet.create({
     paddingHorizontal:15,
     paddingVertical:20,
     marginHorizontal:windowWidth*(1/26),
-    borderRadius:30
+    borderRadius:30,
+    alignItems:'center',
+    justifyContent:'center'
   },
   body:{
     fontSize:28,
     fontWeight:'bold',
-    color:"#dddddd"
+    color:"#dddddd",
+    textAlign:'center'
+  },
+  bodyCard:{
+    height:410,
+    width:'99%',
+    alignItems:'center',
+    justifyContent:'center',
   }
 })
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { SwiperFlatList } from 'react-native-swiper-flatlist';
 import Card from '../components/Card';
 
@@ -9,16 +10,53 @@ import Entypo from 'react-native-vector-icons/Entypo';
 export const windowWidth = Dimensions.get("window").width;
 
 const Home = ({navigation}) => {
-  const numbers = [0, 1, 2, 3, 4, 5]
+  const curiosities = [
+    {
+      id:1,
+      body:`Tom et Jerry s'appelaient à l'origine Jasper et Jinx dans leur premier dessin animé "Puss Gets the Boot"`,
+      images:[1]
+    },
+    {
+      id:2,
+      body:`80% des gens se sont retrouvés à un moment donné à chanter inconsciemment une chanson qu'ils détestent.`,
+      images:[2]
+    },
+    {
+      id:3,
+      body:`De nouvelles recherches ont montré que les personnes seules ont des compétences sociales supérieures par rapport aux personnes qui ne sont pas seules.`,
+      images:[3]
+    },
+    {
+      id:4,
+      body:`Un Rubik's Cube a 43 252 003 274 489 856 000 configurations possibles.`,
+      images:[4]
+    },
+    {
+      id:5,
+      body:`Votre cerveau continue de se développer jusqu'à la quarantaine`,
+      images:[5]
+    },
+    {
+      id:6,
+      body:`Le roi Abdallah d'Arabie Saoudite a été choqué lorsque la reine Elizabeth l'a conduit dans son domaine, car les femmes ne peuvent pas conduire dans son pays.`,
+      images:[6]
+    },
+    {
+      id:7,
+      body:`Soyez une version de premier ordre de vous même, pas une version de second ordre de quelqu'un d'autre.`,
+      images:[7]
+    }
+  ]
   return (
     <ImageBackground source={require("../assets/background.jpg")} style={styles.container}>
-        <SwiperFlatList
+        <FlatList
+          pagingEnabled={true}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.flatList}
-          data={numbers}
-          keyExtractor={item => item}
-          renderItem={({item}) => <Card number={item} />}
+          data={curiosities}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <Card item={item} />}
         />
       <View style={styles.menuTab}>
         {/* <AntDesign style={{padding:3}} name="star" size={26} color="#fff" /> */}
